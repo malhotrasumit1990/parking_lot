51b55dbab1d64b9a021d87547489a9a0ea48755b
continuum-sumit-malhotra <sumit.malhotra@continuum.net>
2019-09-19 00:06:28 +0530

    Up and Ready git status

--- src/businesslayer/constants.go
+++ src/businesslayer/constants.go
@@ -1,7 +1,7 @@
 package businesslayer
 
 const (
-	parkingFull      = "Sorry , Parking is currently full"
+	parkingFull      = "Sorry, parking lot is full\n"
 	parkingAvailable = "Yes , You can park at Parking number %d"
 	parkingEmpty     = "Parking is already empty !!"
 )
--- src/main.go
+++ src/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
@@ -20,60 +23,90 @@ func init() {
 }
 
 func main() {
-	for {
-		var command string
-		var param1 string
-		var param2 string
+	//for {
+	var command string
+	var param1 string
+	var param2 string
 
-		fmt.Scanf("%s %s %s", &command, &param1, &param2)
+	//accepts file path as a flag "filePath"
+	filePath := flag.String("filePath", "", "Read inputs from file")
+	flag.Parse()
 
-		switch command {
-		case "create_parking_lot":
-			capacityInt, err := strconv.Atoi(param1)
-			if err != nil {
-				fmt.Println(err.Error() + "Enter a proper integer for parking numbers")
-				break
-			}
-			dataLayer.CreateParkingSpace(capacityInt)
-			fmt.Printf("Created a parking lot with %d slots\n", capacityInt)
-			break
-		case "park":
-			fmt.Print(parkingLot.ParkVehicle(param1, param2))
-			break
-		case "leave":
-			slot, err := strconv.Atoi(param1)
-			if err != nil {
-				fmt.Println(err.Error() + "Enter a proper integer for slot")
-				break
-			}
-			fmt.Println(parkingLot.UnparkVehicle(slot))
-			break
-		case "status":
-			fmt.Println(parkingLot.GetStatus())
-			break
-		case "registration_numbers_for_cars_with_colour":
-			fmt.Printf("%s \n", parkingLot.GetRegNumsWithColor(param1))
-			break
-		case "slot_numbers_for_cars_with_colour":
-			fmt.Printf("%d \n", parkingLot.GetSlotsForColor(param1))
-			break
-		case "slot_number_for_registration_number":
-			fmt.Printf("%s \n", parkingLot.GetSlotforRegistrationNumber(param1))
-			break
-		case "exit":
-			os.Exit(1)
-		default:
-			fmt.Print("")
-			break
+	//reads from input file
+	if *filePath != "" {
+		file, err := os.Open(*filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Sscan(line, &command, &param1, &param2)
+			executeLogicforParking(command, param1, param2)
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+	} else {
+		//reads from command line
+		//fmt.Scanf("%s %s %s", &command, &param1, &param2)
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Sscan(line, &command, &param1, &param2)
+			executeLogicforParking(command, param1, param2)
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Println(err)
 		}
 	}
 
 }
 
-/*fmt.Print("Enter First String: ") //Print function is used to display output in same line
-var first string
-fmt.Scanln(&first) // Take input from user
-fmt.Print("Enter Second String: ")
-var second string
-fmt.Scanln(&second)
-fmt.Print(first + second)*/
+//}
+func executeLogicforParking(command string, param1 string, param2 string) {
+
+	switch command {
+	case "create_parking_lot":
+		capacityInt, err := strconv.Atoi(param1)
+		if err != nil {
+			fmt.Println(err.Error() + "Enter a proper integer for parking numbers")
+			break
+		}
+		dataLayer.CreateParkingSpace(capacityInt)
+		fmt.Printf("Created a parking lot with %d slots\n", capacityInt)
+		break
+	case "park":
+		fmt.Print(parkingLot.ParkVehicle(param1, param2))
+		break
+	case "leave":
+		slot, err := strconv.Atoi(param1)
+		if err != nil {
+			fmt.Println(err.Error() + "Enter a proper integer for slot")
+			break
+		}
+		fmt.Print(parkingLot.UnparkVehicle(slot))
+		break
+	case "status":
+		fmt.Println(parkingLot.GetStatus())
+		break
+	case "registration_numbers_for_cars_with_colour":
+		fmt.Printf("%s \n", parkingLot.GetRegNumsWithColor(param1))
+		break
+	case "slot_numbers_for_cars_with_colour":
+		fmt.Printf("%d \n", parkingLot.GetSlotsForColor(param1))
+		break
+	case "slot_number_for_registration_number":
+		fmt.Printf("%s \n", parkingLot.GetSlotforRegistrationNumber(param1))
+		break
+	case "exit":
+		os.Exit(1)
+	default:
+		fmt.Print("")
+		break
+	}
+}
